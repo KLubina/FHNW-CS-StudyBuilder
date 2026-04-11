05627302e7f99f81ed787392bc55813a632a1b5b
Kristian Lubina <kristian.lubina@outlook.com>
2026-04-11 19:41:56 +0200

    Simplify subtitleHtml to two links
    Update general-config.js to reduce the subtitleHtml to just the 'Modulübersicht' and 'Modultafel Informatik' links. Removed the 'Allgemeine Info', 'Schwerpunkte Informatik-Fachrichtungen' and 'Studien- und Prüfungsordnung HSI' links and some <br> tags to simplify the rendered subtitle (likely to remove outdated or unnecessary links).

--- study-visualization/program-specific/fhnw-cs-assessment/standard-config/general-config.js
+++ study-visualization/program-specific/fhnw-cs-assessment/standard-config/general-config.js
@@ -6,13 +6,9 @@ window.StudiengangGeneralConfig = {
   title: "BSc Informatik - Nur Assessment",
 
   // Optional HTML subtitle (renders clickable links when provided)
-  subtitleHtml: `<a href="https://www.fhnw.ch/de/studium/informatik/informatik" target="_blank" rel="noopener">Allgemeine Info</a>
-    <br>
+  subtitleHtml: `
     <a href="https://www.fhnw.ch/de/informatik/studium/angebot/studiengaenge/informatik/module-uebersicht" target="_blank" rel="noopener">Modulübersicht</a>
     <br>
     <a href="https://www.fhnw.ch/de/informatik/studium/angebot/studiengaenge/informatik/media/modultabelle.pdf/@@display-file/file" target="_blank" rel="noopener">Modultafel Informatik</a>
-    <br>
-    <a href="https://www.fhnw.ch/de/informatik/studium/angebot/studiengaenge/informatik/media/schwerpunkte_studiumsrichtungen.pdf/@@display-file/file" target="_blank" rel="noopener">Schwerpunkte Informatik-Fachrichtungen</a>
-    <br>
-    <a href="https://www.fhnw.ch/++api++/de/studium/unterstuetzung-rechtliches/formalia/studien-pruefungsordnungen/media/studien-und-pruefungsordnung_hsi_20250915.pdf/@@inline-file/file" target="_blank" rel="noopener">Studien- und Prüfungsordnung HSI</a>`,
+  `,
 };
