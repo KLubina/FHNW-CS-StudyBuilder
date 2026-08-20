5ed4742009f814254362966c6f9ecfe5bfc5d1be
Kristian Lubina <kristian.lubina@outlook.com>
2026-08-20 14:23:13 +0200

    add missing wahl-modules and StudentenHub FHNW link

--- study-visualization/program-specific/standard-config/general-config.js
+++ study-visualization/program-specific/standard-config/general-config.js
@@ -12,5 +12,7 @@ window.StudiengangGeneralConfig = {
     <a href="https://www.fhnw.ch/de/informatik/studium/angebot/studiengaenge/informatik/media/modultabelle.pdf/@@display-file/file" target="_blank" rel="noopener">Modultafel Informatik</a>
     <br>
     <a href="https://sgi.pages.fhnw.ch/moduluebersicht/2026fs/" target="_blank" rel="noopener">Modulübersicht FS 2026</a>
+    <br>
+    <a href="https://studenthub.technik.fhnw.ch/public/student/-1/studienjahrgang/9820253/studienrichtungId/6002493" target="_blank" rel="noopener">Studentenhub FHNW</a>
   `,
 };
